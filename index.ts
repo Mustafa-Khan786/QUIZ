bcdcde4899bf95db03e9e7eddf9872972c55f3d5
GHULAM MUSTAFA <161878014+Mustafa-Khan786@users.noreply.github.com>
2024-07-07 09:47:53 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,55 @@
-
+//Quiz project
 import inquirer from "inquirer";
-const answers =await inquirer.prompt([
-{
-    type:"number",
-    name:"numone",
-    message: "kindly inter your first number",
+import chalk from "chalk"
+const Api="https://opentdb.com/api.php?amount=6&category=18&difficulty=medium";
+let funcget = async (data: string) => {
+  try {
+      let fetchdata = await fetch(data);
+      let res = await fetchdata.json();
+      return res.results || []; // Make sure 'results' property exists, and default to an empty array if it doesn't
+  } catch (error) {
+      console.error("Error fetching data:", error);
+      return [];
+  }
+};
 
-}, 
-{type:"number",
-    name:"numtwo",
-     message: "kindly inter your second number",
 
-},
-{
-    type: "list",
-    name:"opperator",
-    choices: ['addition','subtraction','multiplication',"division"],
-    message:"Sellect your opperator:"
+
+
+let data=await funcget(Api)
+
+
+
+if (data.length === 0) {
+  console.error("No data retrieved. Exiting the program.");
+} else {
+  startquiz();
 }
-]) 
- if (answers.opperator ==="addition") {
-    console.log(answers.numone + answers.numtwo)   
+
+async function startquiz() {
+  let score = 0;
+  let name = await inquirer.prompt({
+      type: "input",
+      name: "Names",
+      message: "What is Your Name",
+  });
+
+  console.log(score);
+
+  for (let i = 0; i < data.length; i++) {
+      let answer = [...data[i].incorrect_answers, data[i].correct_answer];
+      let ans = await inquirer.prompt({
+          type: "list",
+          name: "quiz",
+          message: data[i].question,
+          choices: answer.map((val) => val),
+      });
+
+      if (ans.quiz == data[i].correct_answer) {
+          ++score;
+      }
+  }
+
+  console.log(`Dear ${chalk.green(name.Names)}, Your Score is ${chalk.red(score)} `);
 }
-if (answers.opperator ==="subtraction") {
-    console.log(answers.numone - answers.numtwo)  
-}
-if (answers.opperator ==="multiplication") {
-    console.log(answers.numone * answers.numtwo)
-}
-if (answers.opperator ==="division") {
-            console.log(answers.numone / answers.numtwo)   
-    }
+
